6e8726a421bec5cffb463db7a0103dad8657f236
ArunPrasanth <arunprasanth11456@gmail.com>
2026-04-20 10:20:45 +0530

    Remove outermost parantheses problem solved

---
+++ Remove_outermost_paranthesis/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+
+        int count = 0;
+        String ans = "";
+
+        for(int i = 0; i < s.length(); i++){
+
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+
+            if(count != 0) {
+                ans += s.charAt(i);
+            }
+
+            if(s.charAt(i) == '('){
+                count++;
+            }
+        }
+
+        return ans;
+
+    }
+}
