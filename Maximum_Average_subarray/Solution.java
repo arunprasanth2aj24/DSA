445217c6e02741f9ba1234761127c3e9592637bc
arunprasanth2aj24 <arunprasanth2aj24@kprcas.ac.in>
2026-05-19 09:58:59 +0530

    Maximum average subarray  problem solved

---
+++ Maximum_Average_subarray/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+
+        int sum = 0;
+
+        for(int i = 0; i < k; i++) sum += nums[i];
+
+        int maxSum = sum;
+
+        for(int j = k; j < nums.length; j++){
+
+            sum += nums[j] - nums[j - k];
+            maxSum = Math.max(sum , maxSum);
+        }
+
+        return (double) maxSum / k;
+
+    }
+}
