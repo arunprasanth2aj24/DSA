5cd66c38a62412b4ee0ee8a9926084e088fdb1ae
arunprasanth2aj24 <arunprasanth2aj24@kprcas.ac.in>
2026-05-21 12:30:59 +0530

    Maximum nesting depth of the paranthesis problem solved

---
+++ Maximum_nesting_depth_of_the_Paranthesis/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxDepth(String s) {
+
+        int count = 0;
+        int maxCount = 0;
+
+        for(int i = 0; i < s.length(); i++){
+
+            if(s.charAt(i) == '(') count++;
+            else if (s.charAt(i) == ')') {
+                maxCount = Math.max(maxCount , count);
+                count--;
+            }
+
+        }
+        return maxCount;
+
+    }
+}
